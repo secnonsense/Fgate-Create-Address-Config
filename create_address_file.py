28fd353239f33aeb7b1ae0fe45e25b982b8351eb
Scott Winters <swinters@tns4662l.local>
2018-02-12 09:17:53 -0500

    Add informational message

--- create_address_file.py
+++ create_address_file.py
@@ -15,11 +15,7 @@ if len(sys.argv) < 2:
     print "\nUsage: ./create_address_file.py IPFILE\n"
     quit()
 
-def cidr_to_netmask(cidr):
-    network, net_bits = cidr.split('/')
-    host_bits = 32 - int(net_bits)
-    netmask = socket.inet_ntoa(struct.pack('!I', (1 << 32) - (1 << host_bits)))
-    return network, netmask
+
 =======
 def cidr_to_mask(value):
     
